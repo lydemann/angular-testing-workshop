b3dd6cea36fc788b127ded1c6dbec3655b19f172
Christian Lüdemann <christian@christianlydemann.com>
2022-05-23 22:00:13 +0200

    chore: rename domain

---
+++ libs/shared/domain/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/shared-domain.module';
+export * from './lib/endpoints/endpoints.service';
+export * from './lib/endpoints/endpoints.config';
+export * from './lib/endpoints/endpoints.mock';
+export * from './lib/todo-item';
