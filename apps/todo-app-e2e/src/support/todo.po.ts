229b50d440a343d698bd04d934edb524a67c9f33
Christian Lüdemann <christian@christianlydemann.com>
2022-05-24 00:58:59 +0200

    test: clear po

--- apps/todo-app-e2e/src/support/todo.po.ts
+++ apps/todo-app-e2e/src/support/todo.po.ts
@@ -6,39 +6,15 @@ export class TodoPage {
 	}
 
 	public static createTodoItem() {
-		cy.get('[data-test=todo-title]').type('Some name');
-		cy.get('[data-test=todo-description]').type('Some description');
-		cy.get('[data-test=todo-duedate]').type('2019-10-10');
-
-		cy.get('[data-test=create-todo-submit]').click();
-
-		cy.get('[data-test=todo-item]').should('have.length', 6);
+		// TODO: implement
 	}
 
 	static deleteTodoItem() {
-		cy.get('[data-test=todo-item]').should('have.length', 5);
-		cy.get('[data-test=todo-item]')
-			.first()
-			.shadow()
-			.find('[data-test=delete-button]')
-			.click();
-		cy.get('[data-test=todo-item]').should('have.length', 4);
-		cy.contains(todoList[0].title).should('not.exist');
+		// TODO: implement
 	}
 
 	static editTodoItem() {
-		cy.get('[data-test=todo-item]').should('have.length', 5);
-		cy.get('[data-test=todo-item]')
-			.first()
-			.shadow()
-			.find('[data-test=edit-button]')
-			.click();
-		cy.get('[data-test=todo-title]').clear().type('Edited name');
-		cy.get('[data-test=todo-description]').clear().type('Some description');
-		cy.get('[data-test=todo-duedate]').clear().type('2019-10-10');
-		cy.get('[data-test=create-todo-submit]').click();
-
-		cy.get('[data-test=todo-item]').should('have.length', 5);
+		// TODO: implement
 	}
 
 	public static checkForTodos() {
